545578bf27ec7273bdc0c9b7b386ee3321d9e7ed
Caleb E Klaus <90652137+calebklaus317@users.noreply.github.com>
2021-09-15 15:53:48 -0600

    Add files via upload

--- group.py
+++ group.py
@@ -1,5 +1,14 @@
 def main():
 
+    def calebK(name, year, major, hobbies, grewUp, interesting):
+        name = "My name is Caleb Klaus"
+        year = "Sophmore "
+        major = "CSCI"
+        hobbies = "Biking"
+        grewUp = "California"
+        interesting = "I like to read books"
+        print(name , year , major , hobbies , grewUp , interesting)
+
     def charlesW(name, year, major, hobbies, grewUp, interesting):
         name = "My name is Charles Werk"
         year = "1998 "
