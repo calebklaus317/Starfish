ffc043691eba6b0ff24ddb5c726134a7879c63f2
Charles Werk <79608930+Werk17@users.noreply.github.com>
2021-09-15 15:07:21 -0600

    Update group.py

--- group.py
+++ group.py
@@ -1,13 +1,18 @@
 def main():
 
     def charlesW(name, year, major, hobbies, grewUp, interesting):
-            name = "hello"
-            year = ""
-            major = ""
-            hobbies = ""
-            grewUp = ""
-            interesting = ""
-            return print(name, year, major, hobbies, grewUp, interesting)
-
+        name = "My name is Charles Werk"
+        year = "1998 "
+        major = "CSCI"
+        hobbies = ""
+        grewUp = ""
+        interesting = ""
+        print(name , year , major , hobbies , grewUp , interesting)
+    
 if __name__ == "__main__":
     main()
+        
+
+
+        
+
