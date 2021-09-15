42adb2961e67238cc8cac75ab26dfe7770ac7170
Charles Werk <79608930+Werk17@users.noreply.github.com>
2021-09-15 14:30:18 -0600

    Add files via upload

---
+++ group.py
@@ -0,0 +1,10 @@
+def Groupinfo(name, year, major, hobbies, state, groupplace)
+        print("fGroup member {groupplace}", + name)
+
+    if __name__ = "__main__":
+        main()
+
+
+        def main():
+            print("basic information about the group")
+            Groupinfo(Charles, 4, CSCI,)
